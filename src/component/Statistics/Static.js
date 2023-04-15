30516ab88fb250fb45f4bc47ed156b38ecd20fcb
Nayeemmnt999 <nayeem.mnt999@gmail.com>
2023-04-15 09:00:17 +0600

    added rechart

--- src/component/Statistics/Static.js
+++ src/component/Statistics/Static.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { LineChart, CartesianGrid, XAxis, Line, YAxis } from 'recharts';
 
-const Static = () => {
+const Static = ({quize}) => {
+    console.log(quize.total);
+    const inf = {
+        name: quize.name,
+        uv: quize.total,
+        // pv: data.pv,
+        // amt: data.amt
+    }
     return (
         <div>
-            
+  <LineChart width={500} height={300} data = {quize} >
+                <XAxis dataKey={"name"} />
+                <YAxis />
+                <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+            </LineChart>
         </div>
     );
 };
--- src/component/Statistics/Statistics.js
+++ src/component/Statistics/Statistics.js
@@ -1,21 +1,44 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import { LineChart, CartesianGrid, XAxis, Line } from 'recharts';
-
+import React, { useEffect, useState, } from 'react';
+import axios from 'axios';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 const Statistics = () => {
-    const quizeData = useLoaderData().data;
-    console.log(quizeData);
+
+    const [info, setInfo] = useState([]);
+    // console.log(info);
+    useEffect(() => {
+        axios.get('https://openapi.programming-hero.com/api/quiz')
+            .then(data => {
+                const dataLoder = data.data.data;
+                // console.log(dataLoder.data);
+                // const  dataDataLoader = dataLoder.data;
+
+                const loaded =  dataLoder.map(data => {
+
+                    const inf = {
+                        name: data.name,
+                        uv: data.total,
+                        pv: data.pv,
+                    }
+                    return inf;
+                })
+                setInfo(loaded)
+                // console.log(loaded);
+            })
+    }, [])
+
     return (
-        <div>
-            {
-                
-            }
-            <LineChart width={500} height={300} >
+        <div className=' mt-10'>
+            <LineChart width={1530} height={300} data={info}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+                <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="name" />
-                {/* <YAxis /> */}
-                <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-                <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-                <Line type="monotone" dataKey="pv" stroke="#82ca9d" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" />
+                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                <Line type="monotone" dataKey="amt" stroke="#82ca9d" />
+
             </LineChart>
         </div>
     );
